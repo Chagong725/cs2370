ca965a348123f307e4fde60091baa15d26ec4312
Hyomin Cha <113211090+Chagong725@users.noreply.github.com>
2023-12-03 16:04:41 -0700

    Update named.cpp

--- p6 threads/named.cpp
+++ p6 threads/named.cpp
@@ -10,8 +10,8 @@ const int MAX_NUM = 1000000;
 const int NUM_THREADS = 4;
 
 int primeCounter = 0;
-int sharedCounter = 2;
-vector<int> threadPrimeCount(NUM_THREADS, 0);
+int sharedCounter = 2; //2부터 시작
+vector<int> threadPrimeCount(NUM_THREADS, 0); // 스레드가 찾은 prime mun갯수
 
 ofstream primeFile("primes.dat");
 mutex counterLock, fileLock;
@@ -23,7 +23,7 @@ bool isPrime(int number)
 
     for (int i = 2; i <= number / 2; i++)
     {
-        if (number % i == 0)
+        if (number % i == 0) // remainder확인, quotient는 x
         {
             return false;
         }
@@ -35,18 +35,18 @@ void findPrimes(int threadNum)
 {
     while (true)
     {
-        counterLock.lock();
+        counterLock.lock(); //뮤텍스 잠금으로 다른 스레드가 sharedCounter 못 바꿈
         int candidate = sharedCounter;
         sharedCounter++;
         counterLock.unlock();
 
-        if (candidate > MAX_NUM) break;
+        if (candidate > MAX_NUM) break; 
         if (isPrime(candidate))
         {
             fileLock.lock();
             primeFile << candidate << endl;
             primeCounter++;
-            threadPrimeCount[threadNum]++;
+            threadPrimeCount[threadNum]++; //이 스레드가 찾은 소수 갯수
             fileLock.unlock();
         }
     }
@@ -57,17 +57,17 @@ int main()
     vector<thread> threads;
     for (int i = 0; i < NUM_THREADS; i++)
     {
-        threads.push_back(thread(findPrimes, i));
+        threads.push_back(thread(findPrimes, i)); //fP함수 실행 스레드 벡터의 추가, i는 번호
     }
 
-    for (auto &t : threads)
+    for (auto &t : threads) //오토로 t 타입, &로 스레트 참조 
     {
-        t.join();
+        t.join(); //조인으로 현재 스레드 블록
     }
 
     cout << "Total primes found: " << primeCounter << endl;
 
-    for (int i = 0; i < NUM_THREADS; i++)
+    for (int i = 0; i < NUM_THREADS; i++) //각 스레드 번호와 해당 스레드가 찾은 소수 출력
     {
         cout << "Thread " << i << " found " << threadPrimeCount[i] << " primes" << endl;
     }
